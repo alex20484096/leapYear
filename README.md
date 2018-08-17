23e0c0b19ea2a5614b81faabef18d6e5d2e3aaba
Alexander.Marzan <alexander.marzan@FT-MW4403.local>
2018-08-17 14:41:13 +0100

    2nd commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-echo # leapYear
+# LeapYear
---
+++ isLeapYear.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python3
+
+import sys
+
+def isLeapYear(myYear):
+    if ( myYear % 4 ) == 0:
+        if ( myYear % 100 ) != 0:
+            return "true"
+    else:
+        return "false"
+
+
+
+if len(sys.argv) < 2:
+    print("Please provide a year as an argument")
+else:
+    value = int(sys.argv[1])
+
+    if (isinstance(value,(int)) == True):
+        print(isLeapYear(value))
+    else:
+        print ("Please provide an interger value")
+
+
+#print(len(sys.argv))
+#print(value)
+
+
+#print(isinstance(10.2, (int)))
